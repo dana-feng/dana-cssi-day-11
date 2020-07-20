7b874028b32d8159399cfd67ec0866662264ab3f
Glitch (p5-js-template-cssi-2020) <none>
2020-07-20 18:40:08 +0000

    🤘🏗 Checkpoint ./script.js:18045893/5323

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Be sure to name any p5.js functions we use in the global so Glitch can recognize them.
-// Add to this list as you consult the p5.js documentation for other functions. 
+// Add to this list as you consult the p5.js documentation for other functions.
 /* global createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
           rect, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, 
           mouseX, mouseY, strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke, 
           keyCode, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
 
-let backgroundColor, marcoPosition, rectPosition, mousePosition
+let backgroundColor, marcoPosition, rectPosition, mousePosition, gameOver;
 
 function setup() {
   // Canvas & color settings
@@ -13,53 +13,57 @@ function setup() {
   backgroundColor = 0;
   // This variable contains a JSON object
   marcoPosition = {
-    "x": random(width),
-    "y": random(height)
-  }
-  mousePosition ={
-    "x" : mouseX,
-    "y" : mouseY
-  }
+    x: random(width),
+    y: random(height)
+  };
+  mousePosition = {
+    x: mouseX,
+    y: mouseY
+  };
+  gameOver = false;
 }
 
 function draw() {
-  background(backgroundColor);
-  ellipse(marcoPosition.x, marcoPosition.y, 20, 20); 
-  if(mouseIsPressed){
-    let com = compute(mousePosition, marcoPosition);
-    fill(255);
-    text(`you are ${com}`, 20, 20);
-  }
-}
-
-
-function computeDistance(point1, point2){
-  let deltaX = point1.x - point2.x;
-  console.log(deltaX);
-  let deltaY = point1.y - point2.y;
-  console.log(deltaY);
-  let distance = (deltaX **2 + deltaY **2)**0.5;
-  return distance;
   
+    background(backgroundColor);
+    mousePosition = {
+      x: mouseX,
+      y: mouseY
+    };
+    ellipse(marcoPosition.x, marcoPosition.y, 20, 20);
+    if (mouseIsPressed) {
+      let com = compute(mousePosition, marcoPosition);
+      fill(255);
+      text(`${com}`, 20, 20);
+    }
+  if (gameOver) {
+    background(40);
+    textSize(60);
+    text()
+    
+    
+  }
+  }
+  
+
+function computeDistance(point1, point2) {
+  let deltaX = point1.x - point2.x;
+  let deltaY = point1.y - point2.y;
+  let distance = Math.round((deltaX ** 2 + deltaY ** 2) ** 0.5);
+  return distance;
 }
-function compute(point1, point2){
+function compute(point1, point2) {
   let distance = computeDistance(point1, point2);
   fill(255);
   text(`distance: ${distance}`, 20, 60);
-  if(distance > 200){
-    fill(255);
-    text("cold", 20, 50);
-    return "colder";
-  }
-  else if (distance > 50){
-    fill(255);
-    text("warm", 20, 50);
-    return "warmer";
-    
-  }
-  else{
-    fill(255);
-    text("hot", 20, 50);
-    return "red hot";
+  if (distance > 200) {
+    return "You are cold";
+  } else if (distance > 50) {
+    return "You are warmer";
+  } else if (distance > 10) {
+    return "You are red hot";
+  } else if (distance < 10) {
+    gameOver = true
+    return "YOU WON";
   }
 }
